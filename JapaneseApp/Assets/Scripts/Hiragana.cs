5784e4f07f265238e20e819eaeaf36637f642fd8
Saturno-PC\Saturno <beatriz.s.g86@gmail.com>
2017-04-26 13:15:27 +0200

    Update japaneseApp

---
+++ JapaneseApp/Assets/Scripts/Hiragana.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace JapaneseApp
+{
+	public class HiraganaAlphabet
+	{
+		private string m_Title;
+		public string Title
+		{
+			set { m_Title = value; }
+			get { return m_Title; }
+		}
+
+		private string m_Hiragana;
+		public string Hiragana
+		{
+			set { m_Hiragana = value; }
+			get { return m_Hiragana; }
+		}
+
+		private string m_Romanji;
+		public string Romanji
+		{
+			set { m_Romanji = value; }
+			get { return m_Romanji; }
+		}
+	}
+
+	public class HiraganaData
+	{
+		private List<HiraganaAlphabet> m_HiraganaAlphabet;
+		public List<HiraganaAlphabet> HiraganaAlphabet
+		{
+			get { return m_HiraganaAlphabet; }
+			set { m_HiraganaAlphabet = value; }
+		}
+
+		public HiraganaData()
+		{
+			m_HiraganaAlphabet = new List<HiraganaAlphabet>();
+		}
+	}
+}
+
---
+++ JapaneseApp/Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JapaneseApp
+{
+    public static class Utility
+    {
+        public static List<T> Shuffle<T>(List<T> source)
+        {
+            T[] elements = source.ToArray();
+            List<T> copy = new List<T>();
+            for (int i = elements.Length - 1; i >= 0; i--)
+            {
+                int swapIndex = Random.Range(0, i + 1);
+                copy.Add(elements[swapIndex]);
+                elements[swapIndex] = elements[i];
+            }
+            return copy;
+        }
+
+        public static int Compare(string a, string b)
+        {
+            if (a.Length > b.Length)
+            {
+                return -1;
+            }
+            else if (a.Length < b.Length)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+
+        /// <summary>
+        /// Method to load a JSON with a given path
+        /// </summary>
+        /// <returns>The JSON resource.</returns>
+        /// <param name="pathFile">Path file.</param>
+        public static string LoadJSONResource(string pathFile)
+        {
+            TextAsset text_asset = (TextAsset)Resources.Load(pathFile, typeof(TextAsset));
+            if (text_asset == null)
+            {
+                Debug.Log("ERROR: Could not find file: Assets/Resources/" + pathFile);
+                return "";
+            }
+            string json_string = text_asset.ToString();
+            return json_string;
+        }
+    }
+}
