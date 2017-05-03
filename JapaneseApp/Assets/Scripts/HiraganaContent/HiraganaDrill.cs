f87ae220e264012d5204dbcb12e825c67b07f538
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2017-05-03 16:45:04 +0100

    Added drills

---
+++ JapaneseApp/Assets/Scripts/HiraganaContent/HiraganaDrill.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JapaneseApp
+{
+    public struct DrillUnit
+    {
+        public HiraganaDrill.ETYPEEDRILL TypeQuestion;
+        public int IndexQuestion;
+
+        public HiraganaDrill.ETYPEEDRILL TypeAnswer;
+        //public int IndexAnswer;
+        
+    }
+    public class HiraganaDrill : MonoBehaviour
+    {
+        public enum ETYPEEDRILL { H_SYMBOL, H_ROMANJI, H_WORD, H_WORDROMANJI, H_WORDMEANING };
+
+        private HiraganaData m_HiraganaData;
+
+        private const int m_NumberDrills = 20;
+        private DrillUnit[] m_Drills = new DrillUnit[m_NumberDrills];
+
+        public void Init(HiraganaData data, int idData)
+        {
+            int nUnits = m_NumberDrills / 2;
+
+            // Hiragana symbol questions
+            for (int i = 0; i < nUnits; i++)
+            {
+                m_Drills[i].TypeQuestion = ETYPEEDRILL.H_SYMBOL;
+                m_Drills[i].IndexQuestion = Random.Range(0, data.Hiragana[idData].HiraganaChar.Count);
+                m_Drills[i].TypeAnswer = ETYPEEDRILL.H_ROMANJI;
+            }
+           
+
+        }
+    }
+}
