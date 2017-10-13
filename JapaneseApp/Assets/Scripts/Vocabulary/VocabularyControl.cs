70e25ef92d443a7d3607d265f3079bfb3cf7cce9
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2017-10-13 14:17:33 +0100

    New section

--- JapaneseApp/Assets/Scripts/Vocabulary/VocabularyControl.cs
+++ JapaneseApp/Assets/Scripts/Vocabulary/VocabularyControl.cs
@@ -32,7 +32,7 @@ namespace JapaneseApp
 
     public class VocabularyControl : Base
     {
-        public enum ECategory { NONE = -1, ANIMALS, PROFESIONS, NUMBERS, NUM };
+        public enum ECategory { NONE = -1, ANIMALS, PROFESIONS, NUMBERS, ACTIONS, NUM };
 
         [SerializeField]
         private List<VocabularyData> m_VocabularySet;
