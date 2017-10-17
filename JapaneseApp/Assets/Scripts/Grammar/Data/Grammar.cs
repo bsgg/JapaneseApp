1972b47a43c7b7788218372d1d666452df2509f3
BEA-PC\Bea <beatriz.s.g86@gmail.com>
2017-10-17 16:54:36 +0100

    Include Grammar control

---
+++ JapaneseApp/Assets/Scripts/Grammar/Data/Grammar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JapaneseApp
+{
+    [System.Serializable]
+    public class GrammarSection
+    {
+        [SerializeField]
+        private string m_Title;
+        public string Title
+        {
+            set { m_Title = value; }
+            get { return m_Title; }
+        }
+
+        [SerializeField]
+        private string m_Description;
+        public string Description
+        {
+            set { m_Description = value; }
+            get { return m_Description; }
+        }
+
+        [SerializeField]
+        private string m_Vocabulary;
+        public string Vocabulary
+        {
+            set { m_Vocabulary = value; }
+            get { return m_Vocabulary; }
+        }
+
+
+        [SerializeField]
+        private SentencesExamples m_SentencesExamples = new SentencesExamples();
+        public SentencesExamples SentencesExamples
+        {
+            set { m_SentencesExamples = value; }
+            get { return m_SentencesExamples; }
+        }
+    }
+
+    [System.Serializable]
+    public class GrammarData
+    {
+        [SerializeField]
+        private List<GrammarSection> m_Data = new List<GrammarSection>();
+        public List<GrammarSection> Data
+        {
+            get { return m_Data; }
+            set { m_Data = value; }
+        }
+    }
+
+}
